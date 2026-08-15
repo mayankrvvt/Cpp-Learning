0c96c251d25cbf933874f9244105eda03a0eca25
mayankrvvt <mynkrvvt@gmail.com>
2026-08-16 04:00:51 +0530

    Half Pyramid in Cpp
    Added comments to clarify the purpose of the loops.

--- pattern1.cpp
+++ pattern1.cpp
@@ -5,7 +5,8 @@ int main(){
     int n;
     cout<<"Enter a number : ";
     cin>>n;
-
+    // Outer loop is responsible for the COLUMNS
+    // Inner loop is responsible for the ROWS
     for(int i=1; i<=n; i++){
         for(int j=1; j<=i; j++){
         cout<<"🐱"<<" ";
