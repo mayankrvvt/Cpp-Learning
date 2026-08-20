48f7399a4a439a46991fa6beb6a8ba03f546d2cc
mayankrvvt <mynkrvvt@gmail.com>
2026-08-20 14:43:33 +0530

    array in cpp

---
+++ array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // array = a data structures that can hold multiple values
+    // values are accessed by an index number
+    // "kind of like a variable that hold multiple values"
+
+    string car[] = {"Corvette", "Mustang", "Camry"};
+    cout << car[0];
+    return 0;
+}
