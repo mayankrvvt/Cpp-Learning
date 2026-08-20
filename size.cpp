323bb017271a160ab178434a0716bb5c824686bd
mayankrvvt <mynkrvvt@gmail.com>
2026-08-21 01:35:51 +0530

    Use of sizeof( ) operator in Cpp

---
+++ size.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string name = "Mayank Rawat";
+    double gpa = 2.7;
+
+    cout << sizeof(name) << " bytes" << endl;
+    cout << sizeof(gpa) << " bytes" << endl;
+    return 0;
+}
